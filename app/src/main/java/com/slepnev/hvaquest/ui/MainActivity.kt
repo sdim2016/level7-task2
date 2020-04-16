71e0cd52f41903d568a1bf41cd27c9b7b35e035a
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-16 21:49:25 +0300

    Created the navigation graph, NavHostFragment and NavController

--- app/src/main/java/com/slepnev/hvaquest/ui/MainActivity.kt
+++ app/src/main/java/com/slepnev/hvaquest/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.slepnev.hvaquest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.slepnev.hvaquest.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
     }
+
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the ActionBar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
